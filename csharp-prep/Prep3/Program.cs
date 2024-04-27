ce2e299c432a5d4f1c4b8d44d6ff6f9965a30ad0
xlantz <xlanterman@gmail.com>
2024-04-27 16:51:16 -0600

    I completed the third prep assignment

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,35 @@
 using System;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int magNum = randomGenerator.Next(1, 101);
+
+        int guessNum = 0;
+
+        while (guessNum != magNum)
+        {
+            
+            Console.WriteLine("Guess a number from 1 to 100: ");
+            string strGuessNum = Console.ReadLine();
+
+            guessNum = int.Parse(strGuessNum);
+
+            if (guessNum < magNum)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (guessNum > magNum)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (guessNum == magNum)
+            {
+                Console.WriteLine("Congradulations, you guessed it!");
+            }
+        }
     }
 }
