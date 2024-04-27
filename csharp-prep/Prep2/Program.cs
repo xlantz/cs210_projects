1e563baa663cdc697b23f47ad8931eb48f6b916d
xlantz <xlanterman@gmail.com>
2024-04-27 16:19:14 -0600

    I completed the second prep assignment

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -3,7 +3,44 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep2 World!");
+    {   
+        Console.WriteLine("What is your grade percentage? ");
+        string strGrade = Console.ReadLine();
+
+        int numGrade = int.Parse(strGrade);
+
+        string gradeLetter = "";
+
+        if (numGrade >= 90)
+        {
+            gradeLetter = "A";
+        }
+        else if (numGrade >= 80)
+        {
+            gradeLetter = "B";
+        }
+        else if (numGrade >= 70)
+        {
+            gradeLetter = "C";
+        }
+        else if (numGrade >= 60)
+        {
+            gradeLetter = "D";
+        }
+        else if (numGrade < 60)
+        {
+            gradeLetter = "F";
+        }
+
+        Console.WriteLine(gradeLetter);
+
+        if (numGrade > 70)
+        {
+            Console.WriteLine("You passed!");
+        }
+        else
+        {
+            Console.WriteLine("You failed. Try agin next time.");
+        }
     }
 }
